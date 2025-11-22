f8c623b921fd1af444120588078cbb9a05e520d6
AllaxSydia <aleksej.grishatkin@mail.ru>
2025-11-22 09:43:36 +0500

    Update AuthPage.vue

--- frontend/src/components/Auth/AuthPage.vue
+++ frontend/src/components/Auth/AuthPage.vue
@@ -176,7 +176,7 @@ export default {
   },
   methods: {
     async apiRequest(url, body) {
-      const res = await fetch(`http://localhost:3000${url}`, {
+      const res = await fetch(url, {  // Убрал http://localhost:3000
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
